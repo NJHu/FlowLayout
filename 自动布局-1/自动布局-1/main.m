4a2cf5f856c8a75cce1d6bc0a57dc7af0cb36c66
njhu <64hp@163.com>
2016-07-29 23:11:58 +0800

    解决了移动缩放的基本操作

---
+++ 自动布局-1/自动布局-1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  自动布局-1
+//
+//  Created by 赵帅 on 16/7/29.
+//  Copyright © 2016年 97. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
